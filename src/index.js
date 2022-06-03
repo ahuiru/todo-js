2c4354f8f02f9a612bdefb0cce016179699b0f55
ahuiru <zhuangerlangyou@gmail.com>
2022-06-03 19:00:39 +0900

    add-task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import "./styles.css";
+// import "styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグの生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-low";
+
+  // pタグの生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグの子要素
+  div.appendChild(p);
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  console.log(li)
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li)
+}
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
