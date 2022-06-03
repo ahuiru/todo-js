946bd4e679bc010ae35caaffc2b27474ad42426a
ahuiru <zhuangerlangyou@gmail.com>
2022-06-03 21:30:18 +0900

    complete, incomplete button

--- src/index.js
+++ src/index.js
@@ -16,12 +16,29 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.innerText = inputText;
 
+
+  // 完了ボタン
+  const completeButton = document.createElement("button")
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // 削除ボタン
+  const deleteButton = document.createElement("button")
+  deleteButton.innerText = "削除"
+  deleteButton.addEventListener("click", () => {
+    
+  });
+
   // divタグの子要素
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // liタグの子要素に各要素を設定
   li.appendChild(div);
-  console.log(li)
+
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li)
 }
